e35a9ad7fb3e1b87412da2017649125a9502b568
davinafebry <davinakurniawan14@gmail.com>
2023-07-18 09:39:28 +0700

    echo nav-profile

--- indexUser.php
+++ indexUser.php
@@ -428,7 +428,7 @@ $namaLengkap = $_SESSION['nama_lengkap'];
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
             <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2"><?php echo $username; ?></span>
+            <span class="d-none d-md-block dropdown-toggle ps-2"><?php echo $namaLengkap; ?></span>
           </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
